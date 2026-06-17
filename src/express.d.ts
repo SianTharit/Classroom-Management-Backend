8bb04b24ac73640c5dbd5c07c0e95a17bc45e814
SianTharit <Tharit.preecha1@gmail.com>
2026-06-17 14:19:49 +1000

    Fixed by coderabbit suggested

--- src/express.d.ts
+++ src/express.d.ts
@@ -2,7 +2,7 @@ declare  global {
     namespace Express {
         interface Request {
             user?: {
-                role?: "adin" | "teacher" | "student";
+                role?: UserRoles;
             };
         }
     }
